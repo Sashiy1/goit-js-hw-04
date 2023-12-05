365d76acccc2ab091693d3558bd3b8a81fa4ee7a
Sashiy1 <aleksander.paluch@wp.pl>
2023-12-05 12:54:10 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -9,7 +9,7 @@ const profile = {
   },
 
   getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`
+    return `${this.username} has ${this.playTime} active hours!`;
   },
 };
 
